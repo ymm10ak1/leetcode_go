60194dec05b8698daa3d62c379bdc927ede5e7d3
ymm10 <rihanito30@gmail.com>
2023-08-13 20:41:34 +0900

    [update] "Solutions"で発見した簡潔な方法に更新

--- golang_code/easy_66_plus_one.go
+++ golang_code/easy_66_plus_one.go
@@ -10,31 +10,15 @@ package golangcode
 //
 //lint:ignore U1000 //
 func plusOne(digits []int) []int {
-	lastSize := len(digits) - 1
-    if digits[lastSize] < 9 {
-		digits[lastSize] = digits[lastSize] + 1
-	} else {
-		ans := []int{1}
-		bcnt := make([]bool, len(digits))
-		// 与えられた配列の要素の初期値が9かどうか
-		for i, v := range digits {
-			if v == 9 {
-				bcnt[i] = true
-			}
+	n := len(digits)
+	for i := n-1; i >= 0; i-- {
+		if digits[i] < 9 {
+			digits[i]++
+			return digits
 		}
-		for i := lastSize; i >= 0; i-- {
-			if bcnt[i] {
-				digits[i] = 0
-				if i == 0 {
-					ans = append(ans, digits...)
-					return ans
-				}
-			} else {
-				digits[i] = digits[i] + 1
-				return digits
-			}
-		}
+		digits[i] = 0
 	}
+	digits = append([]int{1}, digits...)
 	return digits
 }
 // @lc code=end
